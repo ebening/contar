7718b61efc663f2d7bc7d18d1c70f2c44788fea1
Edgar <ebening@gmail.com>
2022-03-22 20:22:24 -0600

    Adding BottomNavigationView

--- app/src/main/java/com/iasd/conteo/MainActivity.java
+++ app/src/main/java/com/iasd/conteo/MainActivity.java
@@ -1,14 +1,55 @@
 package com.iasd.conteo;
 
+import static com.iasd.conteo.R.*;
+import static com.iasd.conteo.R.id.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    //ScrollingFragment scrollingFragment = new ScrollingFragment();
+    HomeFragment homeFragment = new HomeFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
+    AssistantsFragment assistantsFragment = new AssistantsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
+
+        bottomNavigationView = findViewById(bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(container, homeFragment).commit();
+
+
+
+        bottomNavigationView.setOnItemSelectedListener(this::onNavigationItemSelected);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    private boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case home:
+                getSupportFragmentManager().beginTransaction().replace(container, homeFragment).commit();
+                return true;
+            case assistant:
+                getSupportFragmentManager().beginTransaction().replace(container, assistantsFragment).commit();
+                return true;
+            case setting:
+                getSupportFragmentManager().beginTransaction().replace(container, settingsFragment).commit();
+                return true;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + item.getItemId());
+        }
     }
 }
